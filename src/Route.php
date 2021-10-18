386e74a0efac306006aa3300d4b623e45f7d7a9d
thgs <theogpl57@gmail.com>
2021-10-18 03:26:35 +0100

    add some todo

--- src/Route.php
+++ src/Route.php
@@ -10,4 +10,7 @@ class Route
     public function __construct(public string $method = '', public string $path = '')
     {
     }
+
+    // @todo do we have to use getArguments on reflection? cant we have an instance of this class?
+    // @see ReflectionAttribute.newInstance
 }
